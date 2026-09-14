34a94a5e1429756551a44d8788d1c00680d5d5f7
icl003 <icl003@bucknell.edu>
2026-09-14 15:06:55 -0400

    chapter 1 first stuff committed 9/14

---
+++ R/str_split_one.R
@@ -0,0 +1,18 @@
+#' Title
+#'
+#' @param x
+#' @param split
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+str_split_one <- function(string, pattern, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
+}
+
